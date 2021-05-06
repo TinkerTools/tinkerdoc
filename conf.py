2efb66d29e30a368d4c2f5d30b86824bdf95a989
Zhi Wang <oukore@gmail.com>
2021-05-06 15:39:29 -0500

    new theme

--- conf.py
+++ conf.py
@@ -44,7 +44,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
+html_theme_options = {
+   'github_url': 'https://github.com/tinkertools',
+   'twitter_url': 'https://twitter.com/tinkertoolsmd'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
